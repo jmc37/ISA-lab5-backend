e8172aeb6399548bfdcfb7b10c34a3e36de02c2f
Joseph Chan <josephchan36@gmail.com>
2023-10-15 14:01:53 -0700

    Added backend routes

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const con = new Client({
   ssl: true, // Enable SSL
 });
 
+con.on('error', (err) => {
+  console.error('PostgreSQL client error:', err);
+});
+
 con.connect()
   .then(() => {
     console.log("Connected to PostgreSQL!");
@@ -25,7 +29,6 @@ function createTable() {
   const sql = "CREATE TABLE IF NOT EXISTS patients (patientid SERIAL PRIMARY KEY, name VARCHAR(100), dateOfBirth DATE)";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
   });
 }
 
@@ -33,9 +36,16 @@ const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
 
   if(req.method === 'GET'){
-    if(pathname === '/patients'){
-      con.query('SELECT * FROM patients', (err, result) => {
-        if (err) throw err;
+    if(pathname === '/patients/'){
+      createTable();
+      const request = query.request
+      con.query(request, (err, result) => {
+        if (err) {
+          console.error('Error executing query:', err);
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+          return;
+        }
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(result.rows));
@@ -45,6 +55,7 @@ const server = http.createServer((req, res) => {
 
   if(req.method === 'POST'){
     if(pathname === '/make'){
+      createTable();
       let data = [
         ['Sara Brown', '1901-01-01'],
         ['John Smith', '1941-01-01'],
@@ -56,14 +67,44 @@ const server = http.createServer((req, res) => {
   
       data.forEach(record => {
         con.query(sql, [record[0], record[1]], function (err, result) {
-          if (err) throw err;
-          console.log("Record inserted");
+          if (err) {
+            console.error('Error inserting record:', err);
+          }
         });
       });
   
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
       res.end('Data inserted!\n');
+    }
+
+    if(pathname === '/add'){
+      createTable();
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString(); // convert buffer to string
+      });
+      req.on('end', () => {
+        try {
+          const data = JSON.parse(body);
+          const request = data.request;
+          con.query(request, (err, result) => {
+            if (err) {
+              console.error('Error executing query:', err);
+              res.statusCode = 500;
+              res.end('Internal Server Error');
+              return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result));
+          });
+        } catch (error) {
+          console.error('Error parsing request body:', error);
+          res.statusCode = 400;
+          res.end('Bad Request');
+        }
+      });
     }
   }
 });
